22b0c1a8199579136feb1b9b7bbd591508dce763
Alfa Indica <indica464@gmail.com>
2025-10-01 08:53:40 +0700

    Add project files.

---
+++ WPFLearn/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WPFLearn
+{
+    public partial class MainWindow : Window
+    {
+        bool isRunning = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (isRunning)
+        //    {
+        //        tbStatus.Text = "Running...";
+        //        btnToggleRun.Content = "Stop";
+        //    }
+        //    else
+        //    {
+        //        tbStatus.Text = "Stopped";
+        //        btnToggleRun.Content = "Start";
+        //    }
+
+        //    isRunning = !isRunning;
+        //}
+    }
+}
---
+++ WPFLearn/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace WPFLearn.View.UserControls
+{
+    public partial class MenuBar : UserControl
+
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+    }
+}

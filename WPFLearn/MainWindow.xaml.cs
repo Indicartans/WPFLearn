887f5558438e3fa1cde0fd790df8cd01ec38da56
Alfa Indica Dzoriful Khazim <indica464@gmail.com>
2025-10-02 10:28:36 +0700

    stackpanel

--- WPFLearn/MainWindow.xaml.cs
+++ WPFLearn/MainWindow.xaml.cs
@@ -8,35 +8,7 @@ namespace WPFLearn
     {
         public MainWindow()
         {
-            DataContext = this;
-            entries = new ObservableCollection<string>();
             InitializeComponent();
-        }
-
-        private ObservableCollection<string> entries;
-
-        public ObservableCollection<string> Entries
-        {
-            get { return entries; }
-            set { entries = value; }
-        }
-
-
-        private void btnAddItem_Click(object sender, RoutedEventArgs e)
-        {
-            Entries.Add(lvInput.Text);
-            lvInput.Clear();
-        }
-
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            string selectedItem = (string)lvEntries.SelectedItem;
-            Entries.Remove(selectedItem);
-        }
-
-        private void btnClear_Click(object sender, RoutedEventArgs e)
-        {
-            Entries.Clear();
         }
     }
 }

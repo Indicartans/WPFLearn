53c86806dc740cdde40b91cfbbf04b193665c8a9
Alfa Indica Dzoriful Khazim <indica464@gmail.com>
2025-10-02 14:54:30 +0700

    style-resources

--- WPFLearn/MainWindow.xaml.cs
+++ WPFLearn/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.ObjectModel;
-using System.Windows;
-using WPFLearn.View;
+﻿using System.Windows;
 
 namespace WPFLearn
 {
@@ -10,25 +7,6 @@ namespace WPFLearn
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void btnNormal_Click(object sender, RoutedEventArgs e)
-        {
-            NormalWindow normalWindow = new NormalWindow();
-            normalWindow.Show();
-        }
-
-        private void btnModal_Click(object sender, RoutedEventArgs e)
-        {
-            ModalWindow modalWindow = new ModalWindow(this);
-            Opacity = 0.5;
-            modalWindow.ShowDialog();
-            Opacity = 1;
-
-            if (modalWindow.Success)
-            {
-                txtInput.Text = modalWindow.Input;
-            }
         }
     }
 }
